4e323cad24a03b926e9d1c822d0ab8784bf5898d
闫兆龙 <yanzhaolong.sh@chinaccs.cn>
2023-11-30 17:22:30 +0800

    页面适配调整

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,29 +12,29 @@ const Footer = () => {
 
   return (
     <footer id="footer">
-      <div className="social">
-        <CustomLink
-          iconDom={<GithubOne />}
-          to={`https://github.com/${githubName}/`}
-        />
-        <CustomLink iconDom={<Home />} to={homeUrl} />
-        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
-      </div>
+      {/*<div className="social">*/}
+      {/*  <CustomLink*/}
+      {/*    iconDom={<GithubOne />}*/}
+      {/*    to={`https://github.com/${githubName}/`}*/}
+      {/*  />*/}
+      {/*  <CustomLink iconDom={<Home />} to={homeUrl} />*/}
+      {/*  <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />*/}
+      {/*</div>*/}
       <div className="text">
-        <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/yanzhaolong/site-status"
-          />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
+        {/*<p>*/}
+        {/*  <CustomLink*/}
+        {/*    text={Package.alia}*/}
+        {/*    to="https://github.com/yanzhaolong/site-status"*/}
+        {/*  />*/}
+        {/*  &nbsp;Version&nbsp;{Package.version}*/}
+        {/*</p>*/}
         <p>
           基于&nbsp;
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          &nbsp;&copy;&nbsp;2023&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
           {/*<CustomLink to="https://www.imsyy.top/" text="無名" />*/}
           {siteIcp ? (
